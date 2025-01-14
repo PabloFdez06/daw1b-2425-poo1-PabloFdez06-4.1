cb51adc072c749a54d59fdb4f185b0ed429589ae
PabloFdez06 <ffpablo@iesrafaelalberti.es>
2025-01-14 12:59:59 +0100

    Ejercicio realizao

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
+
+
 fun main() {
-    println("Hello World!")
+    val rectangulo1 = Rectangulo(4.0, 5.0)
+    println(rectangulo1)
+
+    val rectangulo2 = Rectangulo(5.0, 8.0)
+    println("El rectangulo con base de ${rectangulo2.base} y altura de ${rectangulo2.altura}. Tiene un area de ${rectangulo2.calcularArea()} y un perimetro de ${rectangulo2.calcularPerimetro()}. Perfecto pa una mesa (por la cara)")
+
+    val rectangulo3 = Rectangulo(-1.0, 5.0)
+    println(rectangulo3)
+
+    val rectangulo4 = Rectangulo(1.0, -5.0)
+    println(rectangulo4)
+
+    val rectangulo5 = Rectangulo(-4.0, -5.0)
+    println(rectangulo5)
 }
--- src/Rectangulo.kt
+++ src/Rectangulo.kt
@@ -1,3 +1,15 @@
-class Rectangulo {
-    var nombre = "hola"
+class Rectangulo(val base: Double, val altura: Double) {
+
+    fun calcularArea() : Double = base * altura
+
+    fun calcularPerimetro() : Double = (base * altura) * 2
+
+    init {
+        require(base > 0) {"La base debe ser mayor que 0"}
+        require(altura > 0) {"La altura debe ser mayor que 0"}
+    }
+
+    override fun toString(): String {
+        return "Un rectangulo de base: $base, y altura: $altura. Tiene un area de: ${calcularArea()}, y un perimetro de: ${calcularPerimetro()}."
+    }
 }
